4eab069ab9333b87ea5d871b77d84a91626c3745
Liz Gibbons <gibbon70@msu.edu>
2018-12-31 14:20:46 -0500

    figured out bug in community matrices!!

--- scripts/07.community_data_matrices.pres_abs.R
+++ scripts/07.community_data_matrices.pres_abs.R
@@ -4,13 +4,6 @@
 # 2018 
 
 setwd('~/Documents/GitHub/east_woods_work/')
-
-# these boys arent working for some reason I think my dat.all file is getting changed as I write it 
-# in and out 
-# liz_data <- read.csv('data/liz_data.csv')
-# dat.all <- read.csv('data/dat.all.csv')
-
-# gonna use source for this script because im having problems :( 
 dat.all <- read.csv('data/dat.all.csv')
 library(ape)
 library(reshape2)
@@ -30,41 +23,15 @@ com_mat <- function(dat, tr.ewv4){
 
   for(p in unique(dat$plot)){
     spp_occur <- as.data.frame(table(dat$accepted_name[which(dat$plot == p)]))
-    print(spp_occur)
     for (spp in spp_occur$Var1){
-      print(spp)
       if(!spp %in% dimnames(dat.mat)[[2]]) next
       dat.mat[p, spp] <- 1
     }
   }
   colnames(dat.mat) <- gsub('[-, ]', '_', colnames(dat.mat))
-  return(dat.mat)
-  # Commenting this out right now because I think that the dsicrepancy in 
-  # numb spp per plot I am seeing is due to me only including 
-  # spp in the tree. This is is a test dont keep it you idiot
-  # make sure the lines below are included in final run it will fuck everything else up
-  
-  #names_in_tree <- intersect(tr.ewv4$tip.label, colnames(dat.mat))
-  #dat.mat.out <- dat.mat[,which(colnames(dat.mat) %in% names_in_tree)]
-  #return(dat.mat.out) #
-}
-
-num_spp_per_plot <- function(dat){
-  tally <- data.frame()
-  for( p in unique(dat$plot)){
-    x <- length(unique(dat$accepted_name[which(dat$plot == p)]))
-    rw <- cbind(p, x)
-    print(rw)
-    tally <- rbind(tally, rw)
-  }
-  return(tally)
-}
-
-suspicious <- function(bad_plots, dat){
-  for(p in bad_plots){
-    print(p)
-    print('' %in% dat$accepted_name[dat$plot == p])
-  }
+  names_in_tree <- intersect(tr.ewv4$tip.label, colnames(dat.mat))
+  dat.mat.out <- dat.mat[,which(colnames(dat.mat) %in% names_in_tree)]
+  return(dat.mat.out) #
 }
 
 
@@ -78,14 +45,6 @@ dat.mat.all.18 <- com_mat(dat.18, tr.ewv4)
 
 write.csv(dat.mat.all.07, 'data/dat.mat.all.07.csv')
 write.csv(dat.mat.all.18, 'data/dat.mat.all.18.csv')
-
-tally07 <- num_spp_per_plot(dat.07)
-row.names(tally07) <- tally07$p
-tally07 <- tally07[order(tally07$p),]
-tally07$p <- NULL
-tally07
-temp <- data.frame(rowSums(dat.mat.all.07))
-temp$dat.07_counts <- tally07$x[match(row.names(temp), row.names(tally07))]
 
 
 # OK after some testing I've figured it out. The discrepancy between the original data and 
