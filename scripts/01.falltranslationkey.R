eac830724dab13a78f3cf435ec71e692d634dfd6
lizgib <gibbon70@msu.edu>
2019-06-13 14:16:34 -0500

    more cleaning species pool and tried to look at the ordination data again with tree group
    More removing dead stuff and species that dont make sense. Was trying to incorporate my excel spreadsheet into the code (so I can just read it in from there and update any changes, but having some problems).  Also looking at dominant tree group with respect to the ordination and with the updated species pool. Also looked at only quercus and acer tree groups. Completed envfit analyses

--- scripts/01.falltranslationkey.R
+++ scripts/01.falltranslationkey.R
@@ -9,6 +9,7 @@ setwd('~/Documents/GitHub/east_woods_work/')
 dat.all <- read.csv('data/species/dat.all.csv')
 library(magrittr)
 library(readxl)
+library(dbplyr)
 
 all.spp.sorted <- sapply(dat.all$species, function(x) x) %>%
   unlist %>%
@@ -37,14 +38,17 @@ dat.all$accepted_name <- tnrs_all$Accepted_name[match(dat.all$species, tnrs_all$
 # need to go through and check them manually
 # created file with history of these changes that need to be made called removed_spp.xlsx that will be in my folder on the drive
 
-removed_spp <- read_excel(file.path('data/species/removed_spp.xlsx'), sheet = "Sheet1")
-
+removed_spp <- read_excel(file.path('/Volumes/GoogleDrive/My Drive/East Woods/URF 2018 Gibbons/Data/removed_spp.xlsx'), sheet = "Sheet1")
 # look through the species names (not the accepted ones) and modify the accepted name category based on the value in removed/resolved column 
 # gonna do this the ugly way firs because it is quicker but will make it look nicer later 
-
+removed_spp$`Species Name` <- as.factor(removed_spp$`Species Name`)
 remove_these <- removed_spp$`Species Name`[which(removed_spp$Remove == 'removed')]
 #fix_these <- removed_spp$`Species Name`[which(!is.na(removed_spp$Resolve))] # not working so here comes ugly 
 # ----------------
+
+# dat.all$new_accepted_name <- ifelse(is.na(removed_spp$Resolve[match(dat.all$species, removed_spp$`Species Name`)]), 
+#                                     dat.all$accepted_name, 
+#                                     removed_spp$Resolve[match(dat.all$species, removed_spp$`Species Name`)])
 
 dat.all$accepted_name[grep('Hepatica acutiloba', dat.all$species)] <- 'Hepatica acutiloba'
 dat.all$accepted_name[grep('Impatiens', dat.all$species)] <- 'Impatiens capensis'
@@ -62,7 +66,21 @@ dat.all$accepted_name[grep('Quercus coccinea', dat.all$species)] <- 'Quercus ell
 # after doing this it looks like a lot of these oddball species may have alreadu been removed...
 
 dat.all$accepted_name[which(dat.all$species %in% remove_these)] <- NA
-dat.all$accepted_name[which(dat.all$species %in% remove_these)] <- removed_spp$Resolve[match(removed_spp$`Species Name`,dat.all$species)]
+# dat.all$newcol <- removed_spp$Resolve[match(dat.all$species, removed_spp$`Species Name`)]
+# dat.all$newcol <- as.factor(dat.all$newcol)
+# 
+# for (i in 1:length(dat.all$newcol)){
+#   print(dat.all[i, 'newcol'])
+#   # if(is.na(dat.all$newcol[i])){
+#   #   dat.all$newcol[i] <- dat.all$accepted_name[i]
+#   # }
+#   # else{
+#   #   dat.all$newcol[i] <- dat.all$newcol[i]
+#   # }
+# }
+# # dat.all$new_accepted_name <- ifelse(!is.na(dat.all$newcol), dat.all$newcol, dat.all$accepted_name)
+
+
 
 write.csv(dat.all, 'data/species/dat.all.csv', row.names = F, quote = F)
 
--- scripts/14.ordination.R
+++ scripts/14.ordination.R
@@ -19,46 +19,40 @@ plots.env$MgmtUnit <- ifelse(is.na(plots.env$wooded), "Non-Wooded",
 #---------------
 # script to replace how I calculated the dominant tree group in the fall
 
-#path.2018 = '/Volumes/GoogleDrive/My Drive/East Woods/Inventory 2018/Final Data from AES/Final data (4th round) from AES.10.24.2018/'
-#tree.spring.18 <- read_excel(file.path(path.2018, "18-0073 Morton 2018 Spring Veg Data_AES-edits_Oct-22.xlsx"), sheet = "Tree Layer")
-#tree.spring.18 <- tree.spring.18[,-c(1,2,7,8,9,10)]
-# tree cover = DBH
-#names(tree.spring.18) <- c("plot", "spp.code", "species", "cover")
+dat.all <- read.csv('data/species/dat.all.csv')
+tree07 <- dat.all[which(dat.all$datset == 'T' & dat.all$year == '2007'),]
+tree18 <- dat.all[which(dat.all$datset == 'T' & dat.all$year == '2018'),]
 
-#tree.spring.18 <- tree.spring.18[which(tree.spring.18$plot %in% plots.env$PlotID),]
+tree18$genus <- gsub(' .*', '', tree18$species)
+tree07$genus <- gsub(' .*', '', tree07$species)
+  
+source('~/Documents/GitHub/east_woods_work/scripts/cover.R')
 
-# go through each plot in point_info_GIS
-# for all the trees in each plot, which has the max BA
+tree18$cover <- as.numeric(tree18$cover)
+tree07$cover <- as.numeric(tree07$cover)
+tree18 <- get_plot_cover(tree18)
+tree07 <- get_plot_cover(tree07)
 
-#tree.spring.18$genus <- gsub(' .*', '', tree.spring.18$species)
+tree_type_plots_18 <- data.frame()
+for (plt in unique(tree18$plot)){
+ dom <- max(tree18$spp_percent_total_cover[which(tree18$plot == plt)])
+ tree_type <- unique(tree18$genus[which(tree18$spp_percent_total_cover == dom)])
+ temp <- data.frame(plt, tree_type)
+ tree_type_plots_18 <- rbind(tree_type_plots_18, temp)
+}
 
-#source('~/Documents/GitHub/east_woods_work/scripts/04.cover.R')
+plots.env$DomGenus18 <- tree_type_plots_18$tree_type[match(plots.env$PlotID, tree_type_plots_18$plt)]
 
-#tree.spring.18 <- get_BA(tree.spring.18)
-#tree.spring.18 <- get_plot_cover(tree.spring.18)
+tree_type_plots_07 <- data.frame()
+for (plt in unique(tree07$plot)){
+  dom <- max(tree07$spp_percent_total_cover[which(tree07$plot == plt)])
+  tree_type <- unique(tree07$genus[which(tree07$spp_percent_total_cover == dom)])
+  temp <- data.frame(plt, tree_type)
+  tree_type_plots_07 <- rbind(tree_type_plots_07, temp)
+}
 
+plots.env$DomGenus07 <- tree_type_plots_07$tree_type[match(plots.env$PlotID, tree_type_plots_07$plt)]
 
-# tree_type_plots_18 <- data.frame()
-# for (plt in unique(tree.spring.18$plot)){
-#   dom <- max(tree.spring.18$spp_percent_total_cover[which(tree.spring.18$plot == plt)])
-#   tree_type <- unique(tree.spring.18$genus[which(tree.spring.18$spp_percent_total_cover == dom)])
-#   temp <- data.frame(plt, tree_type)
-#   tree_type_plots_18 <- rbind(tree_type_plots_18, temp)
-# }
-
-#plots.env$DomGenus18 <- tree_type_plots_18$tree_type[match(plots.env$PlotID, tree_type_plots_18$plt)]
-
-# ggplot(data = plots.env, aes(x = lon, y = lat, color = factor(wooded)))+
-#   geom_point() +
-#   #geom_point(aes(ew_plots$lon, ew_plots$lat),  cex = 0.8, pch = 21, col = 'red', bg = 'yellow') +
-#   #geom_point(aes(MixMgmt$lon, MixMgmt$lat), color = 'black', pch = 21, bg = 'darkslategray') +
-#   # geom_point(aes(AnnualBurn$lon, AnnualBurn$lat), color = 'blue3', pch = 21, bg = 'blue') +
-#   xlab('Longitude') +
-#   ylab('Latitude') +
-#   scale_size () +
-#   coord_equal() +
-#   ggtitle('Arboretum Plots')
-# -------------
 
 # first grab only plots in East Woods
 ew_plots <- plots.env[which(plots.env$wooded== 'East Woods'),]
@@ -68,89 +62,101 @@ dat_ew <- dat_ew[which(rowSums(dat_ew) > 3),]
 
 # these are likely wetland plots.. were showing up as outliers on the ordination
 # 2018 
-dat_ew <- dat_ew[which(!row.names(dat_ew) %in% c('R79', 'LL123')),]
-ord_outliers <- ew_plots[which(ew_plots$PlotID %in% c('R79', 'LL123')),]
-ew_plots <- ew_plots[which(ew_plots$PlotID %in% rownames(dat_ew)),]
+# dat_ew <- dat_ew[which(!row.names(dat_ew) %in% c('R79', 'LL123')),]
+# ord_outliers <- ew_plots[which(ew_plots$PlotID %in% c('R79', 'LL123')),]
 # 2007 
-# dat_ew <- dat_ew[which(!row.names(dat_ew) %in% c('Y94', 'FF94')),]
-# ord_outliers <- ew_plots[which(ew_plots$PlotID %in% c('Y94', 'FF94')),]
+dat_ew <- dat_ew[which(!row.names(dat_ew) %in% c('Y94', 'FF94', 'R79')),]
+ord_outliers <- ew_plots[which(ew_plots$PlotID %in% c('Y94', 'FF94', 'R79')),]
+
+ew_plots <- ew_plots[which(ew_plots$PlotID %in% rownames(dat_ew)),]
+
 
 ord_ew <- metaMDS(dat_ew)
 
-# ------------------
-# save the points to file so we can cluster using kmeans
-#write.csv(ord_ew$points, 'data/ord_mat/ord_18_all.csv')
-#write.csv(ord_ew$points, 'data/ord_mat/ord_18_all.csv')
-#ord_pts <- ord_ew$points
-# plot(ord_ew, type = 'n')
-# points(ord_ew, display = 'sites', cex = 0.8, pch = 21, col = 'red', bg = 'yellow')
-# #text(ord_ew, display = 'sites', cex = 0.6)
-# title('EW Plots > 3 spp 2007')
-# 
-# ggplot()+
-#   geom_point(aes(ew_plots$lon, ew_plots$lat),  cex = 0.8, pch = 21, col = 'red', bg = 'yellow') +
-#   geom_point(aes(ord_outliers$lon, ord_outliers$lat), color = 'blue') +
-#   xlab('Longitude') +
-#   ylab('Latitude') +
-#   scale_size () +
-#   coord_equal() +
-#   ggtitle('East Woods Plots with > 3 Species 2007')
-
-
 # Burned Plots / Managed Areas
 
-# hulls <- data.frame(ord_ew$points)
-# hulls$MgmtUnit <- ew_plots$MgmtUnit[which(ew_plots$PlotID %in% row.names(hulls))]
-# 
-# plot(ord_ew, type = 'n')
-# points(ord_ew, display = 'sites', cex = 0.8, pch = 21, col = 'red', bg = 'yellow')
-# title('EW Plots Management 2007')
-# ordiellipse(ord_ew, hulls$MgmtUnit, col = c('cyan4', 'coral1', 'darkgray'), lwd = 2, label= F)
-# ordispider(ord_ew, hulls$MgmtUnit, col = c('cyan3', 'coral3', 'darkgray'), label = T)
-# legend('topleft', legend = unique(hulls$MgmtUnit), col = c('cyan4', 'coral1', 'darkgray'), pch = 21, pt.bg = c('cyan4', 'coral1', 'darkgray'), cex = 0.8)
-# 
-# 
-# ggplot(data = ew_plots, aes(x = lon, y = lat, color = factor(MgmtUnit)))+
-#   geom_point() +
-#   xlab('Longitude') +
-#   ylab('Latitude') +
-#   scale_size () +
-#   coord_equal() +
-#   ggtitle('East Woods Plots Management 2007')
+ew_plots$oakacer18 <- ifelse(ew_plots$DomGenus18 == 'Quercus', "Quercus",
+                         ifelse(ew_plots$DomGenus18 =="Acer", "Acer",
+                                (ifelse(is.na(ew_plots$DomGenus18), 'Other', 'Other'))))
+
+ew_plots$oakacer07 <- ifelse(ew_plots$DomGenus07 == 'Quercus', "Quercus",
+                             ifelse(ew_plots$DomGenus07 =="Acer", "Acer",
+                                    (ifelse(is.na(ew_plots$DomGenus07), 'Other', 'Other'))))
+
+hulls <- data.frame(ord_ew$points)
+hulls$MgmtUnit <- ew_plots$MgmtUnit[which(ew_plots$PlotID %in% row.names(hulls))]
+
+plot(ord_ew, type = 'n')
+points(ord_ew, display = 'sites', cex = 0.8, pch = 21, col = 'red', bg = 'yellow')
+title('EW Plots Management 2007')
+text(ord_ew, 'sites')
+ordiellipse(ord_ew, hulls$MgmtUnit, col = c('cyan4', 'coral1', 'darkgray'), lwd = 2, label= F)
+ordispider(ord_ew, hulls$MgmtUnit, col = c('cyan3', 'coral3', 'darkgray'), label = T)
+legend('topleft', legend = unique(hulls$MgmtUnit), col = c('cyan4', 'coral1', 'darkgray'), pch = 21, pt.bg = c('cyan4', 'coral1', 'darkgray'), cex = 0.8)
+
+
+ggplot(data = ew_plots, aes(x = lon, y = lat, color = factor(MgmtUnit)))+
+  geom_point() +
+  xlab('Longitude') +
+  ylab('Latitude') +
+  scale_size () +
+  coord_equal() +
+  ggtitle('East Woods Plots Management 2018')
 
 
 # Dominant tree type
-# hulls$DomGenus18 <- ew_plots$DomGenus18[which(ew_plots$PlotID %in% row.names(hulls))]
-# 
-# colvec <- c('cornflowerblue', 'cornsilk4', 'aquamarine2', 'blueviolet', 'deeppink3', 'darkgreen', 
-#             'chocolate4', 'deepskyblue3', 'deeppink4', 'darkblue', 'chartreuse2', 'darkseagreen3', 'darkgoldenrod3')
-# 
+hulls$oakacer18 <- ew_plots$oakacer18[which(ew_plots$PlotID %in% row.names(hulls))]
+hulls$oakacer07 <- ew_plots$oakacer07[which(ew_plots$PlotID %in% row.names(hulls))]
+
+colvec <- c('cornflowerblue', 'cornsilk4', 'aquamarine2', 'blueviolet', 'deeppink3', 'darkgreen',
+            'chocolate4', 'deepskyblue3', 'deeppink4', 'darkblue', 'chartreuse2', 'darkseagreen3', 'darkgoldenrod3')
+
 # plot(ord_ew, type = 'n')
-# points(ord_ew, display = 'sites', cex = 0.8, pch = 21, col = colvec[hulls$DomGenus18])
+# points(ord_ew, display = 'sites', cex = 0.8, pch = 21, col = 'red', bg = 'yellow')
 # #text(ord_ew, display = 'sites', cex = 0.6)
 # #ordiellipse(ord_ew, hulls$DomGenus18, col = colvec[hulls$DomGenus18], lwd = 2, label= T)
-# #ordispider(ord_ew, hulls$DomGenus18, col = colvec, label = F)
-# title('Dominant Tree Genus EW plots 2007')
-# legend('topleft', legend = unique(hulls$DomGenus18), col = as.numeric(unique(factor(colvec[hulls$DomGenus18]))), pch = 21, cex = 0.8)
-# 
-# 
-# ggplot(data = ew_plots, aes(x = lon, y = lat, color = factor(DomGenus18)))+
-#   geom_point() +
-#   #geom_point(aes(ew_plots$lon, ew_plots$lat),  cex = 0.8, pch = 21, col = 'red', bg = 'yellow') +
-#   #geom_point(aes(MixMgmt$lon, MixMgmt$lat), color = 'black', pch = 21, bg = 'darkslategray') +
-#   # geom_point(aes(AnnualBurn$lon, AnnualBurn$lat), color = 'blue3', pch = 21, bg = 'blue') +
-#   xlab('Longitude') +
-#   ylab('Latitude') +
-#   scale_size () +
-#   coord_equal() +
-#   ggtitle('Dominant Tree Genus 2007')
+# ordispider(ord_ew, hulls$oakacer18, col = c('chocolate4', 'blueviolet', 'cornflowerblue'), label = F)
+# title('Oak/Acer EW plots 2018')
+# legend('topleft', legend = unique(hulls$oakacer18), col = c('chocolate4', 'blueviolet', 'cornflowerblue'), pch = 21, cex = 0.8)
 
-# this is not gonna be the way I calculated things in the fall
-# for now, just gonna count up which was the dominant
+ggplot(data = hulls, aes(x = hulls$MDS1, y = hulls$MDS2, col = hulls$oakacer07)) + 
+  geom_point(pch = 21, cex = 0.8) + 
+  geom_polygon(data = hulls, aes(MDS1, MDS2, fill = oakacer07, group = oakacer07, alpha = 0.30)) + 
+  coord_equal() + 
+  ggtitle('Oak Acer Plots 2007')
+  theme(
+      axis.text.x = element_blank(),  # remove x-axis text
+      axis.text.y = element_blank(), # remove y-axis text
+      axis.ticks = element_blank(),  # remove axis tickspanel.background = element_blank(), 
+      panel.grid.major = element_blank(),  #remove major-grid labels
+      panel.grid.minor = element_blank())  #remove minor-grid labels)
+  
 
-# ew_plots$PBD <- diversity_metrics$PBD[match(ew_plots$PlotID, diversity_metrics$X)]
-# ew_plots$SR <- diversity_metrics$SR[match(ew_plots$PlotID, diversity_metrics$X)]
-# 
+ew_plots$DomGenus18 <- as.factor(ew_plots$DomGenus18)
+ew_plots$DomGenus07 <-  as.factor(ew_plots$DomGenus07) 
+
+
+n <- 20
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+colvec = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+tregen <- unique(c(as.vector(ew_plots$DomGenus07), as.vector(ew_plots$DomGenus18)))
+
+myColors <- colvec[1:17]
+names(myColors) <- levels(tregen)
+colScale <- scale_colour_manual(name = "tregen", values = myColors)
+
+ggplot(data = ew_plots, aes(x = lon, y = lat, col = ew_plots$DomGenus18, fill = ew_plots$DomGenus07)) +
+   geom_point(shape = 21, size = 3, stroke = 2) +
+   #geom_point(shape = 21, col = ew_plots$DomGenus18, fill = ew_plots$DomGenus07)+
+   xlab('Longitude') +
+   ylab('Latitude') +
+   scale_size () +
+   coord_equal() +
+   ggtitle('Dominant Tree Genus Changes between 2018 + 2007')
+
+
+
 # ggplot(data = ew_plots, aes(x = lon, y = lat, size = SR)) + 
 #   geom_point(col = 'darkblue')+
 #   xlab('Longitude') +
@@ -219,25 +225,26 @@ ord_ew <- metaMDS(dat_ew)
 
 #-------------------
 dat.ord <- as.data.frame(ord_ew$points)
-temp <- ew_plots[,c('aspect', 'elev', 'slope')]
+#temp <- ew_plots[,c('MgmtUnit', 'lat', 'lon')]
+temp <- ew_plots[,c('lat', 'lon', 'oakacer07')]
 
 (fit <- envfit(ord_ew, temp, perm = 999, na.rm = T))
 scores(fit, "vectors")
-plot(ord_ew, 'sites')
 plot(ord_ew, type = 'n')
+plot(ord_ew, 'sites')
 plot(fit, p.max = 0.05, col = "red")
+title('Dominant tree group Features Fit 2007')
 
 ord <- cca(dat_ew ~ MgmtUnit + unit, ew_plots, na.action = na.omit)
-plot(ord, type="p")
-plot(ord, type="s")
+plot(ord, 'sites')
 fit <- envfit(ord, temp, perm = 999, display = "lc", na.rm = T)
 plot(fit, p.max = 0.05, col = "red")
 
-ggplot(data = ew_plots, aes(x = lon, y = lat, size = dat.ord$MDS2, col = dat.ord$MDS2)) +
-  geom_point()+
-  xlab('Longitude') +
-  ylab('Latitude') +
-  scale_size () +
-  coord_equal() +
-  ggtitle('Ordination all plots 2018 (MDS2)')
+# ggplot(data = ew_plots, aes(x = lon, y = lat, size = dat.ord$MDS2, col = dat.ord$MDS2)) +
+#   geom_point()+
+#   xlab('Longitude') +
+#   ylab('Latitude') +
+#   scale_size () +
+#   coord_equal() +
+#   ggtitle('Ordination all plots 2018 (MDS2)')
 
